d32c792d6f9bd76441e383c7d67599bc6f2e185b
Alessio Napoli <alessionapoli99@icloud.com>
2022-02-21 16:11:00 +0100

    Changed package.json private: true

--- src/components/InfoMain.vue
+++ src/components/InfoMain.vue
@@ -2,9 +2,8 @@
   <section class="info">
     <div class="content-info">
       <div v-for="(info,index) in listInfo" :key="index" class="card">
-        <img :src="'../assets/img/' + info.image " :alt="info.name">
+        <img :src="info.image" :alt="info.name">
         <span>{{info.name}}</span>
-        
       </div>
     </div>
   </section>
@@ -18,23 +17,23 @@ export default {
       listInfo:[
         {
           name: "Digital Comics",
-          image: "dc-logo.png"
+          image: "../assets/img/buy-comics-digital-comics.png"
         },
         {
           name: "DC Merchandise",
-          image: "buy-comics-merchandise.png"
+          image: "../assets/img/buy-comics-merchandise.png"
         },
         {
           name: "Subscription",
-          image: "buy-comics-subscriptions.png"
+          image: "../assets/img/buy-comics-subscriptions.png"
         },
         {
           name: "Comic Shop Locator",
-          image: "buy-comics-shop-locator.png"
+          image: "../assets/img/buy-comics-shop-locator.png"
         },
         {
           name: "DC Power Visa",
-          image: "buy-dc-power-visa.png"
+          image: "../assets/img/buy-dc-power-visa.png"
         }
       ]
     }
@@ -43,28 +42,28 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.info{
-  background-color: rgb(5, 130, 249);
-  height: 200px;
-  .content-info{
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    width: 70%;
-    margin: auto;
-    .card{
-      height: 70px;
-      img{
-        height: 100%;
-        vertical-align: middle;
-      }
-      span{
-        color: #fff;
-        text-transform: uppercase;
-        margin-left: 10px;
-        font-size: 13px;
+  .info{
+    background-color: rgb(5, 130, 249);
+    height: 200px;
+    .content-info{
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      width: 70%;
+      margin: auto;
+      .card{
+        height: 70px;
+        img{
+          height: 100%;
+          vertical-align: middle;
+        }
+        span{
+          color: #fff;
+          text-transform: uppercase;
+          margin-left: 10px;
+          font-size: 13px;
+        }
       }
     }
   }
-}
 </style>
